9c68300ea7256ad272c3d97b7d2c4eff460dfacc
lina-jamal <linahjamal89@gmail.com>
2021-07-22 19:00:40 +0300

    ctreate string resultPhrase pase to the totalscore and show it in the app

--- quizapp/lib/result.dart
+++ quizapp/lib/result.dart
@@ -5,13 +5,27 @@ class Result extends StatelessWidget {
   final resultScore;
 
   const Result(this.resetQuize, this.resultScore);
+  String get resultPhrase {
+    String resultText;
+    if (resultScore >= 150) {
+      resultText = 'Wow... You are very awesome';
+    } else if (resultScore > 100) {
+      resultText = 'Very Good ...!';
+    } else if (resultScore > 50) {
+      resultText = 'Pretty likable...!';
+    } else {
+      resultText = 'You are so bad...!';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Column(
       children: [
         Text(
-          "${resultScore}",
+          resultPhrase,
           style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
         // FlatButton(onPressqed: onPressed, child: child)
